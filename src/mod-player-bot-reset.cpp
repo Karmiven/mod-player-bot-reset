b94460e87ffcb0db6f19c23d6321f23f5e366a7f
NoxMax <50133316+NoxMax@users.noreply.github.com>
2025-07-10 22:04:59 -0600

    typo fix

--- src/mod-player-bot-reset.cpp
+++ src/mod-player-bot-reset.cpp
@@ -28,7 +28,7 @@ static bool  g_DebugMode             = false;
 static bool  g_ScaledChance          = false;
 
 // When true, bots at g_ResetBotMaxLevel are reset only after they have accumulated at least
-// g_MinTimePlayed seconds at that level. Bots above g_ResetBotMaxLevel are reset right away
+// g_MinTimePlayed seconds at that level. Bots above g_ResetBotMaxLevel are reset right away.
 static bool  g_RestrictResetByPlayedTime  = false;
 static uint32 g_MinTimePlayed             = 86400;  // in seconds (1 Day)
 static uint32 g_PlayedTimeCheckFrequency  = 864;    // in seconds (default check frequency)
